f6d718034dd0323943742e96dc60bd352541235c
Rabbul Mia <mdr191700@gmail.com>
2025-06-01 00:12:32 +0600

    done my add plant design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import BannerSlider from '../components/Slide/BannerSlider'
+import PlantCard from '../components/PlantCard/PlantCard'
 
 function Home() {
   return (
     <div>
       <BannerSlider />
+      <PlantCard />
     </div>
   )
 }
